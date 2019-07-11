fd5574def4fc2f39099b13f787463233aa2a269a
Rondore <52451751+Rondore@users.noreply.github.com>
2019-07-11 13:02:35 -0600

    Fix formatting on new syscalls

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -412,13 +412,13 @@
 #define __NR_renameat2			(__NR_SYSCALL_BASE+382)
 */
 #define __NR_seccomp			(__NR_SYSCALL_BASE+383)
-#define __NR_getrandom                 (__NR_SYSCALL_BASE+384)
-#define __NR_memfd_create              (__NR_SYSCALL_BASE+385)
-#define __NR_bpf                       (__NR_SYSCALL_BASE+386)
-#define __NR_execveat                  (__NR_SYSCALL_BASE+387)
-#define __NR_userfaultfd               (__NR_SYSCALL_BASE+388)
-#define __NR_membarrier                        (__NR_SYSCALL_BASE+389)
-#define __NR_mlock2    (__NR_SYSCALL_BASE+390)
+#define __NR_getrandom			(__NR_SYSCALL_BASE+384)
+#define __NR_memfd_create		(__NR_SYSCALL_BASE+385)
+#define __NR_bpf			(__NR_SYSCALL_BASE+386)
+#define __NR_execveat			(__NR_SYSCALL_BASE+387)
+#define __NR_userfaultfd		(__NR_SYSCALL_BASE+388)
+#define __NR_membarrier			(__NR_SYSCALL_BASE+389)
+#define __NR_mlock2			(__NR_SYSCALL_BASE+390)
 
 /*
  * This may need to be greater than __NR_last_syscall+1 in order to
